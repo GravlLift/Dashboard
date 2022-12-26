bd25ace178d913bcfe7f585b63cdeca457983237
GravlLift <jason.gravell@gmail.com>
2022-12-26 10:05:05 -0500

    Add main page tabs

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,16 @@
-import { Card, Center, ChakraProvider, Flex, Spinner } from '@chakra-ui/react';
+import {
+  Card,
+  Center,
+  ChakraProvider,
+  Flex,
+  Spinner,
+  Tab,
+  TabList,
+  TabPanel,
+  TabPanels,
+  Tabs,
+} from '@chakra-ui/react';
 import { bind } from '@react-rxjs/core';
-import '@tremor/react/dist/esm/tremor.css';
 import { combineLatest, map, switchMap, timer } from 'rxjs';
 import styles from './App.module.css';
 import CurrentConditions from './components/CurrentConditions/CurrentConditions';
@@ -24,25 +34,35 @@ function App() {
   const forecasts = useForecasts();
   return (
     <ChakraProvider>
-      <Flex height={'100%'}>
-        <Card className={styles.card}>
-          {forecasts.hourly.length && forecasts.current ? (
-            <>
-              <CurrentConditions
-                current={forecasts.current}
-              ></CurrentConditions>
-              <ForecastChart
-                hourlyForecasts={forecasts.hourly}
-                current={forecasts.current}
-              />
-            </>
-          ) : (
-            <Center flexGrow={1}>
-              <Spinner size={'xl'} />
-            </Center>
-          )}
-        </Card>
-      </Flex>
+      <Tabs display={'flex'} flexDir={'column'} height={'100%'} isFitted>
+        <TabPanels flexGrow={1}>
+          <TabPanel height={'100%'}>
+            <Flex height={'100%'}>
+              <Card className={styles.card}>
+                {forecasts.hourly.length && forecasts.current ? (
+                  <>
+                    <CurrentConditions
+                      current={forecasts.current}
+                    ></CurrentConditions>
+                    <ForecastChart
+                      hourlyForecasts={forecasts.hourly}
+                      current={forecasts.current}
+                    />
+                  </>
+                ) : (
+                  <Center flexGrow={1}>
+                    <Spinner size={'xl'} />
+                  </Center>
+                )}
+              </Card>
+            </Flex>
+          </TabPanel>
+        </TabPanels>
+        <TabList>
+          <Tab>Weather</Tab>
+          <Tab>Activities</Tab>
+        </TabList>
+      </Tabs>
     </ChakraProvider>
   );
 }
